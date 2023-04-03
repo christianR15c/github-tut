bd828ddf90543fec713d5eaaddf5366a59bbe190
christian <habinezac15@gmail.com>
2023-04-03 16:26:56 +0200

    ft git error solved

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
+
+dotenv.config()
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {
+app.listen(Process.env.PORT || 4000, () => {
     console.log('Server listening on port 3000');
-});
+})
